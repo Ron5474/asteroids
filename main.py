fe079fc622745eb36f6659ff4506725995449e40
Ronak Patel <rpatelcode@gmail.com>
2024-09-14 20:04:55 -0700

    functioning gui and player movements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+from player import Player
 import pygame
 
 
@@ -7,11 +8,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    p = Player(x, y)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    p.containers = (updatable, drawable)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        p.update(dt)
         screen.fill("black")
+        p.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     print("Starting asteroids!")
